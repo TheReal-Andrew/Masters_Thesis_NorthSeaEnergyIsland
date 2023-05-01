36be87ab160e75d76f3cce2ead615c1b067a5af7
Lukas B. Nordentoft <Lukasbnordentoft@gmail.com>
2023-05-01 09:31:31 +0200

    Preliminary results and gorm updates

--- model/v_2030_preliminary_nac/v_2030_preliminary_nac_results.py
+++ model/v_2030_preliminary_nac/v_2030_preliminary_nac_results.py
@@ -28,9 +28,14 @@ gm.set_plot_options()
 
 solutions = np.load('v_2030_preliminary_nac_3MAA_10p_solutions.npy')
 
+n_opt = pypsa.Network('v_2030_preliminary_nac_opt.nc')
+
 techs = ['P2X', 'Data', 'Store1']
 
-gm.solutions_2D(techs, solutions, n_samples = 10000,
+#%%
+gm.solutions_2D(techs, solutions, n_samples = 100000,
                 title = '2D plot of 3D MAA space, without area constraint',
-                filename = 'v_2030_preliminary_nac_3MAA_10p_plot_2D_MAA.pdf')
+                filename = 'v_2030_preliminary_nac_3MAA_10p_plot_2D_MAA.pdf',
+                )
+
 
