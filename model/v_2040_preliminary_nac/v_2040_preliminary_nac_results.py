3fbf4b396b2b38dc4f93595d488435b02b5c0642
Lukas B. Nordentoft <Lukasbnordentoft@gmail.com>
2023-05-02 09:58:07 +0200

    Set up 2040 preliminary

--- model/v_2040_preliminary_nac/v_2040_preliminary_nac_results.py
+++ model/v_2040_preliminary_nac/v_2040_preliminary_nac_results.py
@@ -1,11 +1,9 @@
 # -*- coding: utf-8 -*-
 """
-Created on Wed Apr 26 13:19:05 2023
+Created on Wed Apr 26 09:23:17 2023
 
 @author: lukas
 """
-
-#%% Import
 
 import os
 import sys
@@ -14,28 +12,37 @@ os.chdir(os.path.join(os.path.dirname(__file__)))
 sys.path.append(os.path.abspath('../../modules')) 
 
 import pypsa
-import numpy as np
+from pypsa.linopt import get_var, linexpr, join_exprs, define_constraints, get_dual, get_con, write_objective, get_sol, define_variables
 import pandas as pd
-from scipy.spatial import ConvexHull
-import matplotlib.pyplot as plt
+import numpy as np
 
 import gorm as gm
 import tim as tm
-from ttictoc import tic,toc
+import pypsa_diagrams as pdiag
+import matplotlib.pyplot as plt
+
 gm.set_plot_options()
 
-#%% Results
+#%%
 
-solutions = np.load('v_2030_preliminary_nac_3MAA_10p_solutions.npy')
-
-n_opt = pypsa.Network('v_2030_preliminary_nac_opt.nc')
-
+solutions = np.load('')
 techs = ['P2X', 'Data', 'Store1']
 
-#%%
-gm.solutions_2D(techs, solutions, n_samples = 100000,
-                title = '2D plot of 3D MAA space, without area constraint',
-                filename = 'v_2030_preliminary_nac_3MAA_10p_plot_2D_MAA.pdf',
+gm.solutions_2D(techs, solutions, n_samples = 1000,
+                title = '2D plot of 3D MAA space',
+                filename = 'v_2040_preliminary_3MAA_10p_plot_2D_MAA.pdf'
                 )
+
+
+
+
+
+
+
+
+
+
+
+
 
 
