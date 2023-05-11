e2a8b0e36e9952fa59d028ab060955c28e7656f1
Lukas B. Nordentoft <Lukasbnordentoft@gmail.com>
2023-05-11 17:41:47 +0200

    MAA setup for results

--- model/v_2030_links_G1/v_2030_links_G1_results.py
+++ model/v_2030_links_G1/v_2030_links_G1_results.py
@@ -35,10 +35,20 @@ name  = f'v_{year}_{study_name}_{len(variables)}MAA_{int(mga_slack*100)}p_'
 title = f'Model: {study_name}_{year}, for {len(variables)} MAA variables, with {int(mga_slack*100)} % slack'
 
 solutions = np.load(name + 'solutions.npy')
+n_opt     = pypsa.Network(f'v_{year}_{study_name}_opt.nc')
 
-gm.solutions_2D(variables, solutions, n_samples = 1000,
+bus_df = tm.get_bus_df()
+
+# filter the rows based on the abbreviations in the 'variables' list
+countries  = bus_df.loc[bus_df['Abbreviation'].isin(variables)]['Bus name'].tolist()
+opt_system = n_opt.links[n_opt.links['bus0'].isin(countries)].p_nom_opt.tolist()
+
+
+#%%
+gm.solutions_2D(variables, solutions, n_samples = 1_000_000,
                 title = title,
-                # filename = f'v_{year}_{study_name}_{len(variables)}MAA_{int(mga_slack*100)}p_plot_2D_MAA.pdf',
+                opt_system = opt_system,
+                filename = f'graphics/v_{year}_{study_name}_{len(variables)}MAA_{int(mga_slack*100)}p_plot_2D_MAA.pdf',
                 )
 
 
